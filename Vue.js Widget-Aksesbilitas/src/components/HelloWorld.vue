44296704337bed0ec7554243c1a45f451f38a2df
fathulhudoyo <fathulhudoyo@animemusic.us>
2024-06-16 15:51:50 +0700

    Sample Version 2.0

---
+++ Vue.js Widget-Aksesbilitas/src/components/HelloWorld.vue
@@ -0,0 +1,114 @@
+
+<style>
+.rowfirst{
+  color:red !important;
+}
+.rowfirstsec{
+  color:yellow !important;
+}
+</style>
+<template>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+    integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://ppid.jakarta.go.id/assets/home/ppidNew/css/bootstrap.min.css" rel="stylesheet">
+  <div class="hello">
+    <h1 style="color: red;">{{ msg }}</h1>
+    <p class="rowfirst rowfirstsec">
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
+          rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
+          rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docsx</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li style="color: green;"><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a>
+      </li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+  </div>
+  <div>
+
+    <span id="loadmodaldisabilitas"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted() {
+
+    const body_1 = document.querySelector("body");
+      const script_1 = document.createElement("script");
+      script_1.setAttribute(
+        "src",
+       "https://code.responsivevoice.org/responsivevoice.js?key=fbcUgt6M"
+      );
+       
+      script_1.async = true;
+      body_1 .appendChild(script_1);
+
+   
+      const body_2 = document.querySelector("body");
+      const script_2 = document.createElement("script");
+      script_2.setAttribute(
+        "src",
+         "https://web.animemusic.us/widget_disabilitas.js"
+      );
+       
+      script_2.async = true;
+      body_2 .appendChild(script_2);
+
+
+
+
+
+
+
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
